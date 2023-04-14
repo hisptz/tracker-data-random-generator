1706007a00d6b94faea4e655d1820c5aa8255a57
gian <nnkogift@hotmail.com>
2023-04-14 15:08:26 +0300

    fix(styling): Fixes
    - Implemented main container max width

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,11 @@ import React from 'react'
 import {MainRouter} from "./modules/Router/Router";
 import "./styles/main.css"
 import "./styles/common.css"
+
 const MyApp = () => (
+    <div className="main-container">
         <MainRouter/>
+    </div>
 )
 
 export default MyApp
