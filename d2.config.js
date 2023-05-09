62d9981feb399829f367a1bcbf5c5dff2b54dce0
gian <nnkogift@hotmail.com>
2023-05-09 12:32:32 +0300

    feat(icons): Implementation
    - Implemented new icon
     - Changed app name

--- d2.config.js
+++ d2.config.js
@@ -1,6 +1,7 @@
 const config = {
     type: 'app',
-    title: 'Random data generator',
+    title: 'Random Data Generator',
+    name: 'dhis2-random-data-generator',
     entryPoints: {
         app: './src/App.tsx',
     },
