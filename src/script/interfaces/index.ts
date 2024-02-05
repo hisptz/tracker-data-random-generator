cd74a8f383d2d7f6fb4a92b40acb621bce45979d
Gift Nnko <nnkogift@hotmail.com>
2024-02-05 16:33:24 +0300

    feat(config): Implementation
    - Added type for event program config
     - Added eslint and prettier

--- src/script/interfaces/index.ts
+++ src/script/interfaces/index.ts
@@ -1,73 +1,89 @@
-import {DHIS2ValueType} from "@hisptz/dhis2-utils";
-import {OrgUnit} from "@hisptz/dhis2-ui/build/types/components/selectors/OrgUnitSelector/types";
-
+import { DHIS2ValueType } from "@hisptz/dhis2-utils";
+import { OrgUnit } from "@hisptz/dhis2-ui/build/types/components/selectors/OrgUnitSelector/types";
 
 export interface SupportedParam {
-		type: 'number' | 'text' | 'options' | 'date' | 'default' | 'object' | 'boolean',
-		label: string;
-		keys?: Array<SupportedParam & { key: string }>
-		options?: { name: string; value: any }[];
-		fieldProps?: Record<string, any>
+	type:
+		| "number"
+		| "text"
+		| "options"
+		| "date"
+		| "default"
+		| "object"
+		| "boolean";
+	label: string;
+	keys?: Array<SupportedParam & { key: string }>;
+	options?: { name: string; value: any }[];
+	fieldProps?: Record<string, any>;
 }
 
 export interface DataType {
-		name: string;
-		fn: any;
-		dhis2Fields: DHIS2ValueType[],
-		defaultParams?: any[];
-		supportedParams?: SupportedParam[]
+	name: string;
+	fn: any;
+	dhis2Fields: DHIS2ValueType[];
+	defaultParams?: any[];
+	supportedParams?: SupportedParam[];
 }
 
 export interface DataItemConfig {
-		dataItemId: string,
-		dataTypeName: string,
-		options?: {
-				deps?: Record<string, any>,
-				probability?: number,
-				params?: any[];
-		}
+	dataItemId: string;
+	dataTypeName: string;
+	options?: {
+		deps?: Record<string, any>;
+		probability?: number;
+		params?: any[];
+	};
 }
 
 export interface TimeBoundary {
-		min?: string,
-		max?: string,
+	min?: string;
+	max?: string;
 }
 
-
 export interface GenerateProps {
-		count: number;
+	count: number;
 }
 
 export interface GenerateWithPaginationProps {
-		pages: number;
-		pageSize?: number
+	pages: number;
+	pageSize?: number;
 }
 
-
 export interface DataConfigurationForm {
-		name: string;
+	name: string;
+	id: string;
+	attributes?: DataItemConfig[];
+	programStages?: {
 		id: string;
-		attributes?: DataItemConfig[],
-		programStages?: {
-				id: string;
-				dataElements: DataItemConfig[]
-		}[]
+		dataElements: DataItemConfig[];
+	}[];
+}
+
+export interface EventProgramDataConfiguration {
+	id: string;
+	count?: number;
+	dataElements: DataItemConfig[];
+	name: string;
+	meta: {
+		orgUnits?: OrgUnit[];
+		locale?: string;
+		eventTimeBoundary: TimeBoundary;
+	};
 }
 
 export interface DataConfiguration {
-		name: string;
+	name: string;
+	id: string;
+	meta: {
+		orgUnits?: OrgUnit[];
+		trackedEntityType?: string;
+		enrollmentTimeBoundary?: TimeBoundary;
+		locale?: string;
+	};
+	attributes?: DataItemConfig[];
+	programStages?: {
+		eventTimeBoundary: TimeBoundary;
 		id: string;
-		meta: {
-				orgUnits?: OrgUnit[]
-				trackedEntityType?: string;
-				enrollmentTimeBoundary?: TimeBoundary;
-				locale?: string;
-		}
-		attributes?: DataItemConfig[],
-		programStages?: {
-				eventTimeBoundary: TimeBoundary;
-				id: string;
-				count?: number;
-				dataElements: DataItemConfig[]
-		}[]
+		count?: number;
+		dataElements: DataItemConfig[];
+	}[];
 }
